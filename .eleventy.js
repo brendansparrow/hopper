6de7aaf2d4de3cf6473e723bcc00dbc89950cbfc
Brendan Sparrow <btsparrow@hotmail.co.uk>
2019-10-31 11:40:46 +0000

    Update eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,28 @@
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
 module.exports = function(eleventyConfig) {
+  // plugins
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addPlugin(pluginSyntaxHighlight);
+
+  // only content in the `posts` directory
+  eleventyConfig.addCollection("posts", function(collection) {
+    return collection.getFilteredByGlob("./_site/posts/*.md").reverse();
+  });
 
   // output
   return {
+    templateFormats: ["md", "njk", "html", "liquid"],
+    pathPrefix: "/",
     htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "liquid",
     dir: {
       input: "./",
       output: "./_site",
       includes: "_includes",
-      layouts: "_layouts"
+      layouts: "_layouts",
+      data: "_data",
     }
   };
 };
