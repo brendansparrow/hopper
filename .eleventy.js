5bf22a902d157091c6d1e2018e5c681c38b1ab1c
Brendan Sparrow <btsparrow@hotmail.co.uk>
2019-11-19 21:27:41 +0000

    Add eleventy-plugin-pwa and update dependencies

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,10 @@
+const pluginPWA = require("eleventy-plugin-pwa");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = function(eleventyConfig) {
   // plugins
+  eleventyConfig.addPlugin(pluginPWA);
   eleventyConfig.addPlugin(pluginRss);
   eleventyConfig.addPlugin(pluginSyntaxHighlight);
 
@@ -11,12 +13,16 @@ module.exports = function(eleventyConfig) {
     return collection.getFilteredByGlob("./_site/posts/*.md").reverse();
   });
 
+  // pass through manifest.json
+  eleventyConfig.addPassthroughCopy("manifest.json");
+
   // output
   return {
     templateFormats: ["md", "njk", "html", "liquid"],
     pathPrefix: "/",
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "liquid",
+    passthroughFileCopy: true,
     dir: {
       input: "./",
       output: "./_site",
