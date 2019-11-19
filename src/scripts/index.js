448c182d0ca70560854572f51caf9bd69c871c78
Brendan Sparrow <btsparrow@hotmail.co.uk>
2019-11-19 21:30:24 +0000

    Register service worker in index.js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,8 @@
-console.log(`Hello World`);
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .then(function() {
+      console.log("ServiceWorker has been registered!");
+    })
+    .catch(console.error);
+}
